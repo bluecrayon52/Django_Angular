ecf68bfa719f98a1cbdc0d888e8eac4f789b416f
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-05-21 21:32:09 -0700

    code tidy

--- login-register/login-reg-client/src/app/app.module.ts
+++ login-register/login-reg-client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
